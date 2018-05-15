bedadccb4113be31a56dd57dccb3d8c7c704c81a
vandalj <jade.vandal@gmail.com>
2018-05-15 11:14:25 +0200

    Modif calculs.c + Ajouts fonctions structure

---
+++ include/structure.h
@@ -0,0 +1,23 @@
+#include "arbres.h"
+
+/*------------------STRUCTURE ARBRE------------------*/
+
+int poids(arbre a);
+int caractere(arbre a);
+
+/*------------------STRUCTURE TABLE------------------*/
+
+int acces_correspondance(table t, int i);
+void modifier_correspondance(table *t, int i, int val);
+int acces_longueur(table t, int i);
+void modifier_longueur(table *t, int i, int val);
+
+
+/*------------------STRUCTURE LECTURE------------------*/
+
+int taille(lecture l);
+int char_dif(lecture l);
+int acces_occurrence(lecture l, int i);
+void modifier_occurrence(lecture *l, int i, int val);
+
+}
