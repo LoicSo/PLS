d99ae8e5a3bb28fb9700c479c5a54438fd4af705
Saxe <guillaume2.denis@gmail.com>
2018-05-15 09:08:27 +0200

    ajout structure

---
+++ include/arbres.h
@@ -0,0 +1,8 @@
+
+typedef struct a {
+	struct a *f_gauche;
+	struct a *f_droite;
+	int poids;
+	int caractere;
+}arbre, *p_arbre;
+
---
+++ include/lecture_ecriture.h
@@ -0,0 +1,8 @@
+#define ASCII 256
+
+typedef struct l{
+	char occurence[ASCII];
+	int taille;
+	int char_dif;
+}lecture, *p_lecture; 
+
