dbabe6ad216cb3e66e812487e7054fb9909616a8
Saxe <guillaume2.denis@gmail.com>
2018-05-16 09:22:18 +0200

    ok

--- include/lecture_ecriture.h
+++ include/lecture_ecriture.h
@@ -16,4 +16,49 @@ typedef struct l{
 	int char_dif;
 }lecture, *p_lecture; 
 
-typedef char* ecriture;
+ char* ecriture;
+
+/**
+ * @brief Permet d'écrire dans un fichier 
+ * @details Prend les données compressées
+ * 
+ * @param nom_fichier un tableau de caractère 
+ */
+void ecrire_fichier (char *nom_fichier);
+
+/**
+ * @brief Permet d'initialiser la structure passé en paramètre
+ * 
+ * 
+ * @param fichier une typedef lecture
+ */
+void initialisation_struct(lecture* fichier);
+
+/**
+ * @brief Permet de renvoyer la longueur d'un fichier 
+ * 
+ * 
+ * @param f fichier de depart
+ * @return la longueur du fichier
+ */
+int longueur_fichier(FILE* f);
+
+/**
+ * @brief Permet d'initialisé la structure depuis un fichier
+ * 
+ * 
+ * @param f fichier de départ
+ * @return une structure de lecture complete
+ */
+lecture lire_fichier(FILE* f);
+
+/**
+ * @brief Permet de transcoder les caractère grace au code de Huffman
+ * 
+ * 
+ * @param t la table correspondant à l'arbre de Huffman
+ * @param l la structure lecture
+ */
+void faire_donnee(p_table t, p_lecture l);
+
+
